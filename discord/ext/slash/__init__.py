28101a3b6da393ba75a0524b01ca1f1657d5ab84
quiktea <wishymovies@gmail.com>
2021-04-14 17:21:42 +0100

    setup.py added

--- discord/ext/slash/__init__.py
+++ discord/ext/slash/__init__.py
@@ -1,1 +1,10 @@
+
+__title__ = 'dpy-ext-slash'
+__author__ = 'quiktea'
+__license__ = 'MIT'
+__version__ = '0.0.1a'
+
+
+
+
 from slash.client import *
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+
+
+
+
+
+
+DESCRIPTION = 'A discord.py slash commands extension'
+
+# Setting up
+setup(
+    name="dpy-slash-ext",
+    version=__version__,
+    author="quiktea",
+    author_email="wishymovies@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=open("README.md").read(),
+    packages=find_packages(),
+    install_requires=['discord.py', 'aiohttp'],
+    keywords=['python', 'discord.py', 'slash commands', 'discord', 'api', 'discord api'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
